341ba154823eacc4e7a7f0e161b7401c84b457ee
GV79 <glam01@uoguelph.ca>
2020-11-12 02:01:52 -0500

    performance enhancements

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
 import Head from 'next/head';
 import Introduction from '../components/Profile/Introduction';
 import Content from '../components/Navigation/Content';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const { head } = document;
+    const fontLink = document.createElement('link');
+    const iconLink = document.createElement('link');
+
+    fontLink.type = 'text/css';
+    fontLink.rel = 'stylesheet';
+    iconLink.type = 'text/css';
+    iconLink.rel = 'stylesheet';
+
+    fontLink.href = 'https://fonts.googleapis.com/css?family=Montserrat:200,300,400,700,900';
+    iconLink.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
+
+    head.appendChild(fontLink);
+    head.appendChild(iconLink);
+
+    return () => {
+      head.removeChild(link);
+    };
+  }, [])
+
   return (
     <div>
       <Head>
@@ -12,11 +34,11 @@ export default function Home() {
         <meta name='keywords' content='Giavinh, Giavinh Lam, Gia Vinh Lam, Gia Vinh, University of Guelph' />
         <meta name='author' content='Giavinh Lam' />
         <link rel='icon' href='/favicon.ico' />
-        <link href='https://fonts.googleapis.com/css?family=Montserrat:200,300,400,700,900' rel='stylesheet' />
+        {/* <link href='https://fonts.googleapis.com/css?family=Montserrat:200,300,400,700,900' rel='stylesheet' />
         <link
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
-        />
+        /> */}
         <link rel='prefetch' href='/docs/giavinhlam-1.png' />
       </Head>
 
