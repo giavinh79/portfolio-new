2a0baa4064a4e5379579f092321dfdf72fb3fd85
GV79 <giavinhlam@gmail.com>
2021-02-05 04:36:13 -0500

    Fix view details button

--- components/Projects/Card.jsx
+++ components/Projects/Card.jsx
@@ -11,9 +11,11 @@ export const Card = ({ description, image, title }) => {
       </Link>
       <p className={styles.title}>{title}</p>
       <p className={styles.description} dangerouslySetInnerHTML={{ __html: description }}></p>
-      <button tabIndex='0' className={styles.button} onClick={() => router.push(`/projects/${title.toLowerCase()}`)}>
-        VIEW DETAILS &rarr;
-      </button>
+      <Link href={`/projects/${urlTitle}`}>
+        <button tabIndex='0' className={styles.button}>
+          VIEW DETAILS &rarr;
+        </button>
+      </Link>
     </div>
   );
 };
