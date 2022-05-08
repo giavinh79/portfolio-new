76e41a8ba892a1a7cfb4658124907981a0f049fc
GV79 <giavinhlam@gmail.com>
2022-05-08 07:14:21 -0400

    add opengraph metadata and update dependencies

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,29 @@ export default function Home() {
         />
         <meta name='keywords' content='Giavinh, GV79, Giavinh Lam, Gia Vinh Lam, Gia Vinh, University of Guelph' />
         <meta name='author' content='Giavinh Lam' />
+
+        {/* Facebook Meta Tags */}
+        <meta property='og:url' content='https://giavinhlam.com/' key='ogurl' />
+        <meta property='og:type' content='website' />
+        <meta property='og:title' content='Giavinh Lam' key='ogtitle' />
+        <meta property='og:site_name' content="Giavinh Lam's Portfolio" key='ogsitename' />
+        <meta
+          property='og:description'
+          content="Giavinh Lam is a software developer working with languages like JavaScript (React.js) and Java. He graduated with a Bachelor's in Computer Engineering at the University of Guelph."
+        />
+        <meta property='og:image' content='https://giavinhlam.com/images/abstract/portfoliosvg.svg' key='ogimage' />
+
+        {/* Twitter Meta Tags */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:domain' content='https://giavinhlam.com' />
+        <meta property='twitter:url' content='https://giavinhlam.com' />
+        <meta name='twitter:title' content='Giavinh Lam' />
+        <meta
+          name='twitter:description'
+          content="Giavinh Lam is a software developer working with languages like JavaScript (React.js) and Java. He graduated with a Bachelor's in Computer Engineering at the University of Guelph."
+        />
+        <meta name='twitter:image' content='https://giavinhlam.com/images/abstract/portfoliosvg.svg' />
+
         <link rel='icon' href='/favicon.ico' />
         <link rel='prefetch' href='/docs/giavinhlam-1.png' />
       </Head>
